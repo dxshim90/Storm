8033efae6160a947d5dd14dd41f882f8ef2e16c3
dxshim90 <dxshim90@gmail.com>
2019-08-03 19:57:43 +0100

    Auth added still a few bugs(Nav button not rerendering and app not rerendering on logout

--- src/Components/AppNav/AppNav.jsx
+++ src/Components/AppNav/AppNav.jsx
@@ -2,26 +2,44 @@ import React from "react";
 import Logo from "../../assets/StormLogo.png";
 import "./AppNac.css";
 import { Button } from "reactstrap";
+import { Link, Redirect } from "react-router-dom";
 
-const AppNav = () => {
-  return (
-    <nav className="navbar navbar-dark bg-dark">
-      <div className="logo">
-        <img src={Logo} width="100" height="80" alt="" />
-      </div>
-      <ul>
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="/employees">Employees</a>
-        </li>
-        <li className="push">
-          <Button>Logout</Button>
-        </li>
-      </ul>
-    </nav>
-  );
-};
+class AppNav extends React.Component {
+  state = {
+    isloggedin: false
+  };
+  logOut = () => {
+    localStorage.removeItem("authuser");
+    this.setState({
+      logout: false
+    });
+  };
+  render() {
+    return (
+      <nav className="navbar navbar-dark bg-dark">
+        <div className="logo">
+          <img src={Logo} width="100" height="80" alt="" />
+        </div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/employees">Employees</Link>
+          </li>
+          <li className="push">
+            {localStorage.authuser ? (
+              <Button onClick={this.logOut}>Logout</Button>
+            ) : (
+              <Link to="/login">
+                <Button className="btn">Login</Button>
+              </Link>
+            )}
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
 
 export default AppNav;
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -14,11 +14,41 @@ class Login extends React.Component {
     });
   };
 
-  onSubmit = () => {};
+  onSubmit = async e => {
+    e.preventDefault();
+    const { username, password } = this.state;
+    const data = {
+      username,
+      password
+    };
+    // const request = await fetch(
+    //   "https://backend-dev.storm-technologies.com/sandbox/v1/token",
+    //   {
+    //     method: "POST",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //       "Access-Control-Origin": "*",
+    //       Accept: "application/json"
+    //     },
+    //     body: JSON.stringify(data)
+    // }
+    //the fetch return a CORS Error, belowis what i expect the server response to look like
+    // );
+    const response = {
+      id: "225",
+      name: "Enthusiastic Candidate",
+      auth_token:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9zaWQiOiI5Mjc2NDdlOC00OWZhLTQ2MTMtOWNmZS1kMzMxZTVmOTA1MmMiLCJyb2xlIjpbInJlYWQiLCJ3cml0ZSJdLCJncm91cHNpZCI6IjQxMzIiLCJ1bmlxdWVfbmFtZSI6IlN0b3JtVGVzdCIsInByaW1hcnlzaWQiOiIyMjUiLCJuYmYiOjE1NjQ4NTI5MjEsImV4cCI6MTU2NDg1NDEyMSwiaWF0IjoxNTY0ODUyOTIxfQ.URSRje-VfjjuilDjGa7hjnnMI8ljV31HZSq8eWp_7m0",
+      expires_in: 1200
+    };
+    const string = JSON.stringify(response);
+    localStorage.setItem("authuser", string);
+    this.props.history.push("/");
+  };
 
   render() {
     return (
-      <div class="login">
+      <div className="login">
         <div>
           <img src={Logo} alt="" />
         </div>
