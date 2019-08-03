9ca2ecc0480a601c53de04547e049c8b925f7223
dxshim90 <dxshim90@gmail.com>
2019-08-03 15:35:44 +0100

    login pages created

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,47 @@
 import React from "react";
+import "./Login.css";
+import Logo from "../../assets/StormLogo.png";
 
-const Login = () => {
-  return <div>Hello from Login Page</div>;
-};
+class Login extends React.Component {
+  state = {
+    username: "",
+    password: ""
+  };
+
+  onChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  onSubmit = () => {};
+
+  render() {
+    return (
+      <div class="login">
+        <div>
+          <img src={Logo} alt="" />
+        </div>
+        <input
+          name="username"
+          type="text"
+          placeholder="Username"
+          id="username"
+          value="StormTest"
+          onChange={this.onChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="password"
+          id="password"
+          value="-!NmmQR2pbmJSQ7"
+          onChange={this.onChange}
+        />
+        <input type="submit" value="Login" onClick={this.onSubmit} />
+      </div>
+    );
+  }
+}
 
 export default Login;
